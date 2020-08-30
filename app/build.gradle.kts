6883a823e4968329ba493eba7761872927f416f3
Kaustubh Patange <developerkp16@gmail.com>
2020-08-30 13:14:51 +0530

    [Removed] PayPal-IAP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -124,7 +124,6 @@ dependencies {
 
     implementation(LibraryDependency.GLIDE)
     implementation(LibraryDependency.CAOC)
-    implementation(LibraryDependency.PAYPAL_IAP)
 
     implementation(LibraryDependency.RXANDROID)
     implementation(LibraryDependency.RXJAVA)
